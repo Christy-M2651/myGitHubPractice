7c54240deae4477df81b3650c7e761568c79f41f
mateus aldi <mateus.aldi@ymail.com>
2020-05-19 19:05:07 -0700

    Add comment about b1 Git branch

--- MateusJavaProject/src/myAppTester.java
+++ MateusJavaProject/src/myAppTester.java
@@ -39,6 +39,14 @@ public class myAppTester {
 		
 		System.out.println();
 		System.out.println("\nYou reach the end of the program.\nGood bye, " + name + "!");
+		
+		/*
+		 * Git now is on branch b1
+		 * from now on until switching out branch b1
+		 * any changes occur here will be tacked in branch b1
+		 * and will not be reflected in any other branch unless b1
+		 * is merged later with that branch
+		 */
 	}
 	
 	/**
